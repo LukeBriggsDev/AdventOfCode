6a35251ba30b15db79b2b2a7efd214107bdc71b1
Luke Briggs <lukebriggs02@gmail.com>
2021-12-01 11:52:47 +0000

    Add solution for day 1

---
+++ 1 - SonarSweep/part_2.py
@@ -0,0 +1,60 @@
+"""
+Considering every single measurement isn't as useful as you expected: there's just too much noise in the data.
+Instead, consider sums of a three-measurement sliding window. Again considering the above example:
+
+199  A      
+200  A B    
+208  A B C  
+210    B C D
+200  E   C D
+207  E F   D
+240  E F G  
+269    F G H
+260      G H
+263        H
+
+Start by comparing the first and second three-measurement windows. 
+The measurements in the first window are marked A (199, 200, 208); their sum is 199 + 200 + 208 = 607.
+The second window is marked B (200, 208, 210); its sum is 618. 
+The sum of measurements in the second window is larger than the sum of the first, so this first comparison increased.
+
+Your goal now is to count the number of times the sum of measurements in this sliding window increases from the previous sum. 
+So, compare A with B, then compare B with C, then C with D, and so on. 
+Stop when there aren't enough measurements left to create a new three-measurement sum.
+
+In the above example, the sum of each three-measurement window is as follows:
+
+A: 607 (N/A - no previous sum)
+B: 618 (increased)
+C: 618 (no change)
+D: 617 (decreased)
+E: 647 (increased)
+F: 716 (increased)
+G: 769 (increased)
+H: 792 (increased)
+
+In this example, there are 5 sums that are larger than the previous sum.
+
+Consider sums of a three-measurement sliding window. How many sums are larger than the previous sum?
+"""
+import os
+
+def get_increases(input_file):
+    input = []
+    with open(input_file) as f:
+        total = 0
+        lines = [int(line.strip()) for line in f]
+        
+        previous_sum = sum(lines[:3])
+
+        for i in range(1, len(lines) - 2):
+            new_sum = sum(lines[i:i+3])
+            if new_sum > previous_sum:
+                total += 1
+            previous_sum = new_sum
+
+        return total
+        
+    
+if __name__ == "__main__":
+    print(get_increases(os.path.dirname(__file__)+"/input.txt"))
