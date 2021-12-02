659937c35f911853ebd1f73ef3843d796db6a5ae
Luke Briggs <lukebriggs02@gmail.com>
2021-12-02 11:18:57 +0000

    Add solutions for day 2

---
+++ 2 - Dive/part_1.py
@@ -0,0 +1,53 @@
+"""
+--- Day 2: Dive! ---
+
+Now, you need to figure out how to pilot this thing.
+
+It seems like the submarine can take a series of commands like forward 1, down 2, or up 3:
+
+    forward X increases the horizontal position by X units.
+    down X increases the depth by X units.
+    up X decreases the depth by X units.
+
+Note that since you're on a submarine, down and up affect your depth, and so they have the opposite result of what you might expect.
+
+The submarine seems to already have a planned course (your puzzle input). You should probably figure out where it's going. For example:
+
+forward 5
+down 5
+forward 8
+up 3
+down 8
+forward 2
+
+Your horizontal position and depth both start at 0. The steps above would then modify them as follows:
+
+    forward 5 adds 5 to your horizontal position, a total of 5.
+    down 5 adds 5 to your depth, resulting in a value of 5.
+    forward 8 adds 8 to your horizontal position, a total of 13.
+    up 3 decreases your depth by 3, resulting in a value of 2.
+    down 8 adds 8 to your depth, resulting in a value of 10.
+    forward 2 adds 2 to your horizontal position, a total of 15.
+
+After following these instructions, you would have a horizontal position of 15 and a depth of 10. (Multiplying these together produces 150.)
+
+Calculate the horizontal position and depth you would have after following the planned course. What do you get if you multiply your final horizontal position by your final depth?
+"""
+import os
+def get_pos(input_file):
+    current_pos = {"x": 0, "y": 0}
+    with open(input_file) as f:
+        for line in f:
+            command = line.strip().split(" ")
+            print(command)
+            if command[0] == "forward":
+                current_pos["x"] += int(command[1])
+            elif command[0] == "down":
+                current_pos["y"] += int(command[1])
+            elif command[0] == "up":
+                current_pos["y"] -= int(command[1])
+    return current_pos["x"] * current_pos["y"]
+
+if "__main__" == __name__:
+    print(get_pos(os.path.dirname(__file__) + "/input.txt"))
+
